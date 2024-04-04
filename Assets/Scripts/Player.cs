d62e07a66354603d9d6d2263143179dad552ead8
Jihyun0126 <dbwlgus0126@gmail.com>
2024-04-04 19:23:11 +0900

    플레이어 빌드업
    이동 알고리즘 및 근접공격 알고리즘 구현, 뒤로 도는 애니메이션 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    public Transform pos;
+    public Vector2 boxSize;
+    public float curTime;
+    public float coolTime = 0.5f;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        curTime = 0;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+        if (curTime <= 0)
+        {
+            Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
+            foreach (Collider2D collider in collider2Ds)
+            {
+                Debug.Log(collider.tag);
+            }
+            anim.SetTrigger("atk");
+            curTime = coolTime; // 쿨다운을 다시 설정
+
+        }
+        else
+        {
+            curTime -= Time.deltaTime; // 쿨다운 시간 감소
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0) // inputVec.x 값이 0보다 큰 경우
+        {
+            if (inputVec.x < 0)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+        }
+    }
+
+    void OnDrawGizmos() // 근접공격 영역설정
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(pos.position, boxSize);
+    }
+}
