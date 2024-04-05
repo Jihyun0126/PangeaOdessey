006789f20774743aff1d69b32bd79b471dbcbe8f
Jihyun0126 <dbwlgus0126@gmail.com>
2024-04-05 16:49:50 +0900

    나무 방패 완성
    나무 방패 3개 돌아감, 텍스쳐하향

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,16 +10,28 @@ public class Weapon : MonoBehaviour
     public int count;
     public float speed;
 
+    void Start()
+    {
+        Init();
+    }
+
     void Update()
     {
-        
+        switch (id)
+        {
+            case 0:
+                transform.Rotate(Vector3.back * speed * Time.deltaTime);
+                break;
+            default:
+                break;
+        }
     }
     public void Init()
     {
         switch (id)
         {
             case 0:
-                speed = -150;
+                speed = 150;
                 Batch();
                 break;
             default:
@@ -32,6 +44,11 @@ public class Weapon : MonoBehaviour
         {
             Transform bullet =GameManager.instance.pool.Get(prefabId).transform;
             bullet.parent = transform;
+
+            Vector3 rotVec = Vector3.forward * 360 * index / count;
+            bullet.Rotate(rotVec);
+            bullet.Translate(bullet.up * 1.9f, Space.World);
+
             bullet.GetComponent<Bullet>().Init(damage, -1); // -1은 계속 관통
         }
     }
