509da3a05e1131721ed265b8f7f65a12e1e71c0e
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-04-05 16:49:31 +0900

    zx
    zx

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,13 +6,14 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D target;
-
+    Animator anim;
     bool isLive = true;
     Rigidbody2D rigid;
     SpriteRenderer spriter;
 
     void Awake()
     {
+        anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
         spriter = GetComponent<SpriteRenderer>();
     }
@@ -25,7 +26,10 @@ public class Enemy : MonoBehaviour
         rigid.MovePosition(rigid.position + nextVec);
         rigid.velocity = Vector2.zero;
     }
-
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Collision");
+    }
     void LateUpdate()
     {
         spriter.flipX = target.position.x < rigid.position.x;
