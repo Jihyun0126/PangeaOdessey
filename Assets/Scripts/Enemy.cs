1ca6d015338f9f31e9df6ac6d47ae8a2b6cdc041
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-04-05 17:28:23 +0900

    azz
    azx

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,10 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D target;
-    Animator anim;
+    public Animator anim;
+    public LayerMask playerLayer; // 플레이어 레이어 마스크
+    public float detectionRadius = 5f; // 공격 감지 범위
+
     bool isLive = true;
     Rigidbody2D rigid;
     SpriteRenderer spriter;
@@ -21,15 +24,31 @@ public class Enemy : MonoBehaviour
     void FixedUpdate()
     {
         if (!isLive) return;
+
         Vector2 dirVec = target.position - rigid.position;
         Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
         rigid.velocity = Vector2.zero;
+
+        // 플레이어 감지 및 공격
+        DetectPlayerAndAttack();
     }
-    public void OnTriggerEnter(Collider other)
+
+    void DetectPlayerAndAttack()
     {
-        Debug.Log("Trigger Collision");
+        // 플레이어 감지
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, detectionRadius, playerLayer);
+        foreach (var hit in hits)
+        {
+            if (hit.CompareTag("Player"))
+            {
+                // 공격 모션 재생
+                anim.SetTrigger("Attack");
+                break; // 하나의 플레이어만 감지하면 루프 종료
+            }
+        }
     }
+
     void LateUpdate()
     {
         spriter.flipX = target.position.x < rigid.position.x;
