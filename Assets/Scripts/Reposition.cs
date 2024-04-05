4b91cd483d715ae1d95ccbc3e5a30886eed43621
Songgihyeon <cj9263@naver.com>
2024-04-05 22:38:13 +0900

    무한맵 추가중
    내일 추가로 하겠습니다

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerExit2D(Collider2D collision)
     {
-        
-    }
+        if (!collision.CompareTag("Area"))
+            return;
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float diffX = Mathf.Abs (playerPos.x - myPos.x);
+        float diffY = Mathf.Abs (playerPos.y - myPos.y);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+        switch (transform.tag) {
+            case "Ground":
+                if (diffX > diffY) {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffX < diffY) {
+                    transform.Translate(Vector3.up * dirY * 40);
+                } 
+                break;
+            case "Enemy":
+
+                break;
+        }      
     }
 }
