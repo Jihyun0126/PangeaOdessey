85c96434d1386173b906ac885587efd59de2bd15
Songgihyeon <cj9263@naver.com>
2024-04-18 09:50:08 +0900

    BossControll 추가
    개발 예정

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public Rigidbody2D target;
+    public Animator anim;
+    public LayerMask playerLayer; // �÷��̾� ���̾� ����ũ
+    public float detectionRadius = 5f; // ���� ���� ����
+
+    bool isLive = true;
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (!isLive) return;
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = Vector2.zero;
+
+        // �÷��̾� ���� �� ����
+        DetectPlayerAndAttack();
+    }
+
+    void DetectPlayerAndAttack()
+    {
+        // �÷��̾� ����
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, detectionRadius, playerLayer);
+        foreach (var hit in hits)
+        {
+            if (hit.CompareTag("Player"))
+            {
+                // ���� ��� ���
+                anim.SetTrigger("Attack");
+                break; // �ϳ��� �÷��̾ �����ϸ� ���� ����
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        spriter.flipX = target.position.x < rigid.position.x;
     }
 }
