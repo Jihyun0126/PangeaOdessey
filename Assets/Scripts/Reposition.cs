757121c30df3b32a3449f3db7fe0f2902633d60b
Songgihyeon <cj9263@naver.com>
2024-05-19 12:34:14 +0900

    무한맵 스크립트 수정, 타일 수정
    진행중

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,28 +9,45 @@ public class Reposition : MonoBehaviour
     {
         if (!collision.CompareTag("Area"))
             return;
-            
+
         Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 myPos = transform.position;
-        float diffX = Mathf.Abs (playerPos.x - myPos.x);
-        float diffY = Mathf.Abs (playerPos.y - myPos.y);
+        
+        /*
+        float diffX = Mathf.Abs(playerPos.x - myPos.x);
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
 
         Vector3 playerDir = GameManager.instance.player.inputVec;
         float dirX = playerDir.x < 0 ? -1 : 1;
         float dirY = playerDir.y < 0 ? -1 : 1;
+        */
 
-        switch (transform.tag) {
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+
+        float diffX = Mathf.Abs(dirX);
+        float diffY = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+        switch (transform.tag)
+        {
             case "Ground":
-                if (diffX > diffY) {
+                if (Mathf.Abs(diffX - diffY) <= 0.1f) {
+                    transform.Translate(Vector3.up * dirY * 40);
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffX > diffY) {
                     transform.Translate(Vector3.right * dirX * 40);
                 }
                 else if (diffX < diffY) {
                     transform.Translate(Vector3.up * dirY * 40);
-                } 
+                }
                 break;
             case "Enemy":
 
                 break;
-        }      
+        }
     }
 }
