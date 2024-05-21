fc8b43dff6308a629216121b5699d5a8e3cad62e
Songgihyeon <cj9263@naver.com>
2024-05-21 21:15:27 +0900

    1

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -1,49 +1,35 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RePosition : MonoBehaviour
+public class Reposition : MonoBehaviour
 {
-    Collider2D coll;
-    void Awake()
-    {
-        coll = GetComponent<Collider2D>();
-    }
     void OnTriggerExit2D(Collider2D collision)
     {
-        
         if (!collision.CompareTag("Area"))
             return;
+            
         Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 myPos = transform.position;
-        float dirX = playerPos.x - myPos.x;
-        float dirY = playerPos.y - myPos.y;
-        float diffx = Mathf.Abs(dirX);
-        float diffy = Mathf.Abs(dirY);
+        float diffX = Mathf.Abs (playerPos.x - myPos.x);
+        float diffY = Mathf.Abs (playerPos.y - myPos.y);
 
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
 
-
-
-
-        dirX = dirX > 0 ? 1 : -1;
-        dirY = dirY > 0 ? 1 : -1;
-        switch (transform.tag)
-        {
+        switch (transform.tag) {
             case "Ground":
-                if (diffx > diffy)
-                {
+                if (diffX > diffY) {
                     transform.Translate(Vector3.right * dirX * 40);
                 }
-                else if (diffx < diffy)
-                {
+                else if (diffX < diffY) {
                     transform.Translate(Vector3.up * dirY * 40);
-                }
+                } 
                 break;
             case "Enemy":
-                
-                break;
 
-        }
+                break;
+        }      
     }
 }
