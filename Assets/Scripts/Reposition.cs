53744c353bca611b98908cf4d1bf22b7ca4828f7
Songgihyeon <cj9263@naver.com>
2024-05-25 23:50:04 +0900

    Revert "Merge branch 'feature_sgh' of https://github.com/Jihyun0126/PangeaOdessey into feature_sgh"
    This reverts commit 02e36e2dd56cf82a06e91a574f0b48f05d15a8ed, reversing
    changes made to c5b724db7e6ece95ebd23954f9eae73b1faaffca.

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -1,35 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Reposition : MonoBehaviour
+public class RePosition : MonoBehaviour
 {
+    Collider2D coll;
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
     void OnTriggerExit2D(Collider2D collision)
     {
+        
         if (!collision.CompareTag("Area"))
             return;
-            
         Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 myPos = transform.position;
-        float diffX = Mathf.Abs (playerPos.x - myPos.x);
-        float diffY = Mathf.Abs (playerPos.y - myPos.y);
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+        float diffx = Mathf.Abs(dirX);
+        float diffy = Mathf.Abs(dirY);
 
-        Vector3 playerDir = GameManager.instance.player.inputVec;
-        float dirX = playerDir.x < 0 ? -1 : 1;
-        float dirY = playerDir.y < 0 ? -1 : 1;
 
-        switch (transform.tag) {
+
+
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+        switch (transform.tag)
+        {
             case "Ground":
-                if (diffX > diffY) {
+                if (diffx > diffy)
+                {
                     transform.Translate(Vector3.right * dirX * 40);
                 }
-                else if (diffX < diffY) {
+                else if (diffx < diffy)
+                {
                     transform.Translate(Vector3.up * dirY * 40);
-                } 
+                }
                 break;
             case "Enemy":
-
+                
                 break;
-        }      
+
+        }
     }
 }
