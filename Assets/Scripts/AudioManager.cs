f579e7330f77ff8fc05fdff1b81e81938c76ca7f
seoseunghyun1025 <seunghyun.seo101@gmail.com>
2024-06-03 00:38:34 +0900

    몬스터 사운드 추가
    몬스터 사운드 추가

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    [Header("#BGM")]
+    public AudioClip bgmClip;
+    public float bgmVolume;
+    AudioSource bgmPlayer;
+
+    [Header("#SFX")]
+    public AudioClip[] sfxClips;
+    public float sfxVolume;
+    public int channels;
+    AudioSource[] sfxPlayers;
+    void Awake()
+    {
+        instance = this;
+        Init();
+    }
+}
