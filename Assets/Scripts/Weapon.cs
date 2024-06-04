b0d7c0496768168d8003120561259d17981542cd
Jihyun0126 <dbwlgus0126@gmail.com>
2024-06-04 19:13:49 +0900

    오류 해결완료

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -113,6 +113,8 @@ public class Weapon : MonoBehaviour
             Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
             bullet.parent = transform;
 
+            bullet.localPosition = Vector3.zero;
+
             Vector3 rotVec = Vector3.forward * 360 * index / count;
             bullet.Rotate(rotVec);
             bullet.Translate(bullet.up * 2f, Space.World);
