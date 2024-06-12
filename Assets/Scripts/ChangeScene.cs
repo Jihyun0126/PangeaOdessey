803da71c5ae295a895c59353bf7626afc7b670de
JinhoBa <127280695+JinhoBa@users.noreply.github.com>
2024-06-12 22:06:18 +0900

    각 씬과 스테이지 버튼 연결
    각 씬과 스테이지 버튼 연결,
    UI 인스펙터창 무기 오브젝트 추가

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -20,6 +20,34 @@ public class ChangeScene : MonoBehaviour
     {
         SceneManager.LoadScene("MainScene");
     }
+
+    public void GrassScene(){
+        SceneManager.LoadScene("Grass");
+    }
+    public void GrassBossScene(){
+        SceneManager.LoadScene("GrassBoss");
+    }
+
+    public void IceScene(){
+        SceneManager.LoadScene("Ice");
+    }
+    public void IceBossScene(){
+        SceneManager.LoadScene("IceBoss");
+    }
+
+    public void FireScene(){
+        SceneManager.LoadScene("Fire");
+    }
+    public void FireBossScene(){
+        SceneManager.LoadScene("FireBoss");
+    }
+
+    public void DarkScene(){
+        SceneManager.LoadScene("Dark");
+    }
+    public void DarkBossScene(){
+        SceneManager.LoadScene("DarkBoss");
+    }
     
 
 }
